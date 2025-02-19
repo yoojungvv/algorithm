44de473c67cc77bac5fd68cac811f032dbc0b88b
Lea <dbwjdvva@gmail.com>
2025-02-19 22:27:49 +0900

    [D3] Title: 준홍이의 카드놀이, Time: 131 ms, Memory: 30,080 KB -BaekjoonHub

--- SWEA/D3/7102. 준홍이의 카드놀이/준홍이의 카드놀이.java
+++ SWEA/D3/7102. 준홍이의 카드놀이/준홍이의 카드놀이.java
@@ -1,21 +1,23 @@
-
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = Integer.parseInt(sc.nextLine());
+        int T = sc.nextInt();
         int testCase = 1;
         while(testCase<=T) {
-            String[] input = sc.nextLine().split(" ");
-            int N = Integer.parseInt(input[0]);
-            int M = Integer.parseInt(input[1]);
+            int N = sc.nextInt();
+            int M = sc.nextInt();
 
             System.out.print("#"+testCase);
-            for(int i=Math.min(N+1,M+1); i<=Math.max(N+1, M+1); i++) {
-                System.out.print(" "+i);
+            if(M<=N) {
+                int temp = M;
+                M = N;
+                N = temp;
             }
+            for(int i=N+1; i<=M+1; i++) System.out.print(" "+i);
             System.out.println();
+
             testCase++;
         }
     }
