672ed3b28a6bae75d44f5cee8e35cd5c30931e3f
Yoojung KIM <dbwjdvva@gmail.com>
2025-03-05 17:43:51 +0900

    [Bronze IV] Title: 녹색거탑, Time: 168 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int result = (int) Math.pow(2, N);
+    System.out.println(result);
+    }
+}
