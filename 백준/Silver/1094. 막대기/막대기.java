be1f775e136b3e3f38b5689fd9c102af0a9065d3
Yoojung KIM <dbwjdvva@gmail.com>
2025-03-10 00:25:13 +0900

    [Silver V] Title: 막대기, Time: 176 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int X = sc.nextInt();
+    System.out.println(Integer.bitCount(X));
+    }
+}
