17418f36bf6df98b50aa014cd7e9e51a8b2eb578
Yoojung KIM <dbwjdvva@gmail.com>
2025-04-02 10:36:24 +0900

    [Silver III] Title: 바이러스, Time: 180 ms, Memory: 17792 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,59 +1,54 @@
-
 import java.util.*;
 
 public class Main {
 
+    static ArrayList<ArrayList<Integer>> computers;
     static boolean[] visited;
-    static List<List<Integer>> graph;
     static int cnt;
 
-    public static void addEdge(int u, int v) {
-        graph.get(u).add(v);
-        graph.get(v).add(u);
-    }
+    static void bfs(int node) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(node);
+        visited[node] = true;
 
-    public static void dfs(int node) {
-        Stack<Integer> st = new Stack<>();
-        st.push(node);
-
-        while(!st.isEmpty()) {
-            int next = st.pop();
-            if(!visited[next]) {
-                cnt++;
-                visited[next] = true;
-            }
-
-            for(int n : graph.get(next)) {
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            for(int n : computers.get(curr)) {
                 if(!visited[n]) {
-                    st.push(n);
+                    q.add(n);
+                    visited[n] = true;
+                    cnt++;
                 }
             }
+
         }
     }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int comNum = Integer.parseInt(sc.nextLine());
         int N = Integer.parseInt(sc.nextLine());
+        int M = Integer.parseInt(sc.nextLine());
 
-        visited = new boolean[comNum+1];
-        graph = new ArrayList<>();
-        cnt = -1;
+        computers = new ArrayList<>();
+        visited = new boolean[N+1];
+        cnt = 0;
 
-        for(int i=0; i<comNum+1; i++) {
-            graph.add(new ArrayList<>());
+        for(int i=0; i<N+1; i++) {
+            computers.add(new ArrayList<>());
         }
 
-        for(int i=0; i<N; i++) {
+        for(int i=0; i<M; i++) {
             String[] input = sc.nextLine().split(" ");
             int u = Integer.parseInt(input[0]);
             int v = Integer.parseInt(input[1]);
-            addEdge(u, v);
+
+            computers.get(u).add(v);
+            computers.get(v).add(u);
         }
 
-        dfs(1);
-
-        System.out.print(cnt);
+        bfs(1);
+        System.out.println(cnt);
 
     }
 }
